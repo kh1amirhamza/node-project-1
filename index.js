0cad1fa08442a9a8f2f76cfb64055fdeee2b46c8
kh7amirhamza <kh7amirhamza@gmail.com>
2020-09-27 15:14:13 +0600

    code update

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var http = require('http');
 //var admin = require('firebase-admin');
 //var adminInit = false;
 var server = http.createServer(function (req, res) {
-    res.write('Welcome');
+    res.writeHead(200,{'Content-Type':'text/html'})
+    res.write('<h1>hfdj</h1>');
     res.end();
 
  /*   if (req.url=="/"){
